38be5694dde59fc7855a05ae98c42fd56b1c406d
MateoBaLa <mateo.barcolargo@kaust.edu.sa>
2025-02-03 16:36:40 +0300

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -7,7 +7,8 @@ import xarray as xr
 
 #Open dataset == "C:\Users\mbarc\geo_env\N21E039.SRTMGL1_NC.nc"
 dset = xr.open_dataset(r'C:\Users\mbarc\geo_env\N21E039.SRTMGL1_NC.nc')
-#Stop code and allow python script in the console
+
+#Stop code and allow python script in the console if necessary 
 #pdb.set_trace()
 
 #NumPy array to understand data extend, values, and type
